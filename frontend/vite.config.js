fdcaf7fe706e635663e9fcaf637beb1807f28a3e
zahirabalqis <zahiranurbalqis@gmail.com>
2025-01-14 06:05:42 +0800

    Add build script and configure Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify output directory for build files
-  },
-  server: {
-    host: true, // Optional: enable external access (for dev)
-    port: 5173,
+    outDir: 'dist',
   },
 });
